4517c0370303c5d7ef7d3742cb9b2b63c69d5741
UdaraDewshan <dewudara@gmail.com>
2025-08-20 01:18:19 +0530

    Add API

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,10 +1,33 @@
-console.log("js loaded");
-
 const video = document.getElementById("bgVideo");
 video.playbackRate = 0.5;
 
-AOS.init();
+AOS.init();//animation
 
+//-------callApi--------
+async function callApi() {
+    await fetch("http://api.weatherapi.com/v1/forecast.json?key=d2e08be9b1a4476e9f7131914251908&q=panadura&days=1&aqi=no&alerts=no")
+        .then(responce => responce.json())
+        .then(data => console.log(data))
+}
+callApi();
 
+//--------setDate-----------
+let date = new Date();
+formatDate = {
+    "weekday": `long`,
+    "month": `long`,
+    "year": `numeric`,
+    "day": `numeric`
+}
+let currentDate = date.toLocaleDateString("en-US", formatDate);
+document.getElementById("date").innerText = currentDate;
 
+//--------searchCity--------------
+let form = document.getElementById("searchForm");
+let searchCity = document.getElementById("text");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    console.log(searchCity.value);
+});
 
